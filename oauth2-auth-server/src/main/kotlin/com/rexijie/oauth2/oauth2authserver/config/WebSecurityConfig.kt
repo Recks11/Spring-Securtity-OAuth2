6becacf7f36152874c354c5f40888f5db08710a3
Rex Ijiekhuamen <dev.rexijie@gmail.com>
2021-08-15 22:47:28 +0100

    mod

--- oauth2-auth-server/src/main/kotlin/com/rexijie/oauth2/oauth2authserver/config/WebSecurityConfig.kt
+++ oauth2-auth-server/src/main/kotlin/com/rexijie/oauth2/oauth2authserver/config/WebSecurityConfig.kt
@@ -1,24 +1,16 @@
 package com.rexijie.oauth2.oauth2authserver.config
 
-import com.rexijie.oauth2.oauth2authserver.repository.AccessTokenRepository
-import com.rexijie.oauth2.oauth2authserver.repository.RefreshTokenRepository
 import com.rexijie.oauth2.oauth2authserver.service.CustomUserDetailsService
 import org.springframework.boot.web.servlet.FilterRegistrationBean
 import org.springframework.context.annotation.Bean
-import org.springframework.context.annotation.Configuration
-import org.springframework.context.annotation.Primary
 import org.springframework.http.HttpMethod
 import org.springframework.security.authentication.AuthenticationManager
-import org.springframework.security.authentication.dao.DaoAuthenticationProvider
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter
 import org.springframework.security.config.http.SessionCreationPolicy
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder
-import org.springframework.security.crypto.password.PasswordEncoder
-import org.springframework.security.oauth2.provider.token.DefaultTokenServices
-import org.springframework.security.oauth2.provider.token.TokenStore
 import org.springframework.web.cors.CorsConfiguration
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource
 import org.springframework.web.filter.CorsFilter
@@ -31,8 +23,8 @@ class WebSecurityConfig(
 
     override fun configure(http: HttpSecurity) {
         http.authorizeRequests()
-                .antMatchers("/auth/token", "/profile/me").permitAll()
-                .antMatchers("/auth/authorize").authenticated()
+                .antMatchers("/oauth/token", "/profile/me").permitAll()
+                .antMatchers("/oauth/authorize").authenticated()
                 .antMatchers("/api/**").authenticated()
                 .antMatchers("/h2-console/**").permitAll()
                 .and()
